9df4281167506219b2f4dacc8449b4b79fd70ff0
Siyovush <paraparadox@protonmail.com>
2022-09-29 13:09:05 +0500

    Golang Web Services: Errors

---
+++ golang-web-services2/errors-handling/own_err.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	client = http.Client{
+		Timeout: time.Duration(time.Millisecond),
+	}
+)
+
+type ResourceError struct {
+	URL string
+	Err error
+}
+
+func (re *ResourceError) Error() string {
+	return fmt.Sprintf("Resource error: URL: %S, err: %v", re.URL, re.Err)
+}
+
+func getRemoteResource() error {
+	url := "http://127.0.0.1:9999/pages?id=123"
+	_, err := client.Get(url)
+	if err != nil {
+		return &ResourceError{
+			URL: url,
+			Err: err,
+		}
+	}
+	return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := getRemoteResource()
+	if err != nil {
+		switch err.(type) {
+		case *ResourceError:
+			err := err.(*ResourceError)
+			fmt.Printf("resource %s err: %s\n", err.URL, err.Err)
+			http.Error(w, "remote resource error", 500)
+		default:
+			fmt.Printf("internal error: %+v\n", err)
+			http.Error(w, "internal error", 500)
+		}
+		return
+	}
+	w.Write([]byte("all is OK"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :4005")
+	http.ListenAndServe(":4005", nil)
+}
